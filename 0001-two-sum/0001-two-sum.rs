70af898ef4546ded79ac2cdf9957dabd08d165d1
Junhyeok Yoo <87534997+suwonyoo@users.noreply.github.com>
2025-02-06 02:07:19 +0900

    Time: 0 ms (100%), Space: 2.8 MB (20.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.rs
+++ 0001-two-sum/0001-two-sum.rs
@@ -1,14 +1,18 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut answer: Vec<i32> = Vec::new();
-        
-        for (i, &val1) in nums.iter().enumerate() {
-            for j in (i + 1)..nums.len() {
-                if val1 + nums[j] == target {
-                    return vec![i as i32, j as i32];
-                }
+        let mut map = HashMap::new();
+
+        for (i, &num) in nums.iter().enumerate() {
+            let to_check_num = target - num;
+
+            if let Some(&j) = map.get(&to_check_num) {
+                return vec![j as i32, i as i32];
             }
+            map.insert(num, i);
         }
         vec![]
+
     }
 }
